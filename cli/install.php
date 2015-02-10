bf6d1375691c0318ad381fd24451b17f8c0e9ce6
ddniel16 <ddniel16@gmail.com>
2015-02-10 19:52:15 +0100

    fidex-bug y mejoras

--- cli/install.php
+++ cli/install.php
@@ -26,7 +26,7 @@ if (empty($host)) {
     $host = 'localhost';
 }
 
-$application = file_get_contents('../application/configs/application.ini');
+$application = file_get_contents('./application/configs/application.ini');
 
 $application = str_replace('{appnamespace}', $namespace, $application);
 $application = str_replace('{dbname}', $dbname, $application);
@@ -34,4 +34,37 @@ $application = str_replace('{username}', $username, $application);
 $application = str_replace('{password}', $password, $application);
 $application = str_replace('{host}', $host, $application);
 
-file_put_contents('../application/configs/application.ini', $application);
+file_put_contents('./application/configs/application.ini', $application);
+
+$mysqli = new mysqli($host, $username, $password, $dbname);
+
+if ($mysqli->connect_errno) {
+    echo "Fallo al contenctar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    return false;
+}
+
+$klearUsers = "
+CREATE TABLE `KlearUsers` (
+  `userId` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(40) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pass` varchar(80) NOT NULL COMMENT '[password]',
+  `active` tinyint(1) DEFAULT '1',
+  `createdOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`),
+  UNIQUE KEY `login` (`login`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='[entity]';";
+
+
+$userAdmin = "insert into KlearUsers (login, pass, active) values ('admin','$2a$08$0hHHBX8So9JhU0a0SNnRCeAZcMEdAfn7T/pl/u/pESzBwztldhRnO', 1);";
+
+if ($mysqli->query($klearUsers)) {
+    $mysqli->query($userAdmin);
+} else {
+    echo "Fallo la creación de KlearUsers \n";
+}
+
+
+
+
