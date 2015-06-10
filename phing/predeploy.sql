49daa3a131c65127665a1d0e1ddb0fd7328471fc
ddniel16 <ddniel16@gmail.com>
2015-06-10 16:25:16 +0200

    agregar la tabla EtagVersions al predeploy

--- phing/predeploy.sql
+++ phing/predeploy.sql
@@ -8,3 +8,11 @@ CREATE TABLE changelog (
 ) COMMENT '[ignore]';
 
 ALTER TABLE changelog ADD CONSTRAINT Pkchangelog PRIMARY KEY (change_number, delta_set);
+
+CREATE TABLE `EtagVersions` (
+    `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+    `table` varchar(55) DEFAULT NULL,
+    `etag` varchar(255),
+    `lastChange` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
