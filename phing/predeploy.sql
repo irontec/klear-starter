2f03a4d7df74fbed5d191e33114dfdcf19e329c2
ddniel16 <ddniel16@gmail.com>
2016-03-15 11:02:08 +0100

    bug-fixed comentario para el phing

--- phing/predeploy.sql
+++ phing/predeploy.sql
@@ -12,7 +12,7 @@ ALTER TABLE changelog ADD CONSTRAINT Pkchangelog PRIMARY KEY (change_number, del
 CREATE TABLE `EtagVersions` (
     `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
     `table` varchar(55) DEFAULT NULL,
-    `etag` varchar(255),
+    `etag` varchar(255) DEFAULT NULL,
     `lastChange` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='[entity]';
