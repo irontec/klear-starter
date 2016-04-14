b05704e1b75d2beab9ddd3203a19a34065157ebb
ddniel16 <ddniel16@gmail.com>
2016-04-14 17:03:59 +0200

    cambios en la definicion

--- web/tests/application/controllers/IndexControllerTest.php
+++ web/tests/application/controllers/IndexControllerTest.php
@@ -5,10 +5,14 @@ class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
 
     public function setUp()
     {
-        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+
+        $this->bootstrap = new Zend_Application(
+            APPLICATION_ENV,
+            APPLICATION_PATH . '/configs/application.ini'
+        );
+
         parent::setUp();
+
     }
 
-
 }
-
--- web/tests/bootstrap.php
+++ web/tests/bootstrap.php
@@ -1,23 +1,42 @@
 <?php
 
-// Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+$baseDir = realpath(__DIR__ . '/../');
 
-// Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'testing'));
+/**
+ * Define path to application directory
+ */
+$applicationPath = $baseDir . '/application';
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', $applicationPath);
 
-// Ensure library/ is on include_path
+/**
+ * Define application environment
+ */
+$env = (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production');
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', $env);
+
+/**
+ * Composer autoloader
+ */
+$vendorDir = $baseDir . '/vendor/';
+if (file_exists($vendorDir)) {
+    require_once $vendorDir . 'autoload.php';
+}
+
+/**
+ * Ensure library/ is on include_path
+ */
+$library = $baseDir . '/library';
+$zendAutoload = $vendorDir . 'zendframework/zendframework1/library';
 set_include_path(
     implode(
         PATH_SEPARATOR,
         array(
-            realpath(APPLICATION_PATH . '/../library'),
-        get_include_path()
+            $library,
+            $zendAutoload
         )
     )
 );
 
+/** Zend Autoloader */
 require_once 'Zend/Loader/Autoloader.php';
-Zend_Loader_Autoloader::getInstance();
+\Zend_Loader_Autoloader::getInstance();
